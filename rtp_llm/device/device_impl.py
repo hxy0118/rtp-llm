889947a0785ac10580a1f7e2b207f736fd9b22cb
linchuchang.lcc <linchuchang.lcc@alibaba-inc.com>
2026-04-14 19:20:58 +0800

    fix USE_SWIZZLEA=1 bug

--- rtp_llm/device/device_impl.py
+++ rtp_llm/device/device_impl.py
@@ -949,11 +949,15 @@ class RocmImpl(GpuImpl):
         if key in [
             W.attn_qkv_w,
             W.attn_o_w,
+            W.attn_gate_w,
             W.ffn_w2,
             W.ffn_w13,
             W.ffn_w3,
             W.moe_gate,
             W.multi_tokens_predict_eh_proj,
+            W.linear_attn_qkvz_w,
+            W.linear_attn_ba_w,
+            W.linear_attn_out_w,
         ]:
             if self.py_env_configs.py_hw_kernel_config.use_swizzleA:
                 if (
